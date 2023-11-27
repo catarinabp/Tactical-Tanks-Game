40be78b9a8c02c8e7d694a5d903d9819a615e23e
Mariana Melo <marianamelo@MBPdeMariana5.lan>
2023-11-27 00:04:29 +0000

    final

---
+++ src/test/java/org/example/controllers/MapControllerTest.java
@@ -0,0 +1,41 @@
+package com.aor.hero.controller;
+
+
+import org.example.GUI.GUI;
+import org.example.model.Position;
+import org.example.controller.game.PlayerTankController;
+import org.example.model.game.elements.PlayerTank;
+import org.example.model.game.map.Map;
+import org.example.model.game.map.RandomMapBuilder;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapControllerTest {
+
+    @Test
+    void PlayerTankStaysWithinBounds() {
+
+        int width = 10;
+        int height = 10;
+        RandomMapBuilder rab = new RandomMapBuilder(width, height, 0);
+        Map map = rab.createMap();
+        PlayerTankController controller = new PlayerTankController(map);
+
+        PlayerTankController spyController = Mockito.spy(controller);
+
+
+        GUI.ACTION[] actions = {GUI.ACTION.UP, GUI.ACTION.RIGHT, GUI.ACTION.DOWN, GUI.ACTION.LEFT};
+
+        for (GUI.ACTION action : actions) {
+            Mockito.doNothing().when(spyController).step(Mockito.any(), Mockito.eq(action), Mockito.anyLong());
+            spyController.step(null, action, 100);
+        }
+
+
+        Position heroPosition = spyController.getModel().getPlayerTank().getPosition();
+        assertTrue(heroPosition.getX() >= 0 && heroPosition.getX() < width);
+        assertTrue(heroPosition.getY() >= 0 && heroPosition.getY() < height);
+    }
+}
---
+++ src/test/java/org/example/controllers/PlayerTankControllerTest.java
@@ -0,0 +1,31 @@
+package org.example.controller;
+
+import org.example.Game;
+import org.example.GUI.GUI;
+import org.example.controller.game.PlayerTankController;
+import org.example.model.Position;
+import org.example.model.game.elements.NPCs;
+import org.example.model.game.map.Map;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerTankControllerTest {
+
+    @Test
+    void testMovePlayerUp() {
+
+        Map map = mock(Map.class);
+        PlayerTankController playerTankController = new PlayerTankController(map);
+        Position initialPosition = new Position(1, 1);
+        playerTankController.getModel().getPlayerTank().setPosition(initialPosition);
+
+        playerTankController.movePlayerUp();
+
+        Position expectedPosition = new Position(0, 1);
+        assertEquals(expectedPosition, playerTankController.getModel().getPlayerTank().getPosition());
+    }
+
+    // Adicione mais testes para os outros métodos conforme necessário
+}
